80ec6b67ebf189e92b249bfd3e7aaac4777e2115
wesinoil <ver96005@byui.edu>
2023-01-11 14:49:21 -0500

    finished prep 3

--- csharp-prep/Prep3/Program.cs
+++ csharp-prep/Prep3/Program.cs
@@ -4,6 +4,31 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep3 World!");
+        
+        Console.WriteLine("Guess the magic number between 1 - 100");
+        Random randomGenerator = new Random();
+
+        int magicNumber = randomGenerator.Next(1, 101);
+        int guess = -1;
+
+        while (guess != magicNumber)
+        {
+            Console.WriteLine("What is your guess? ");
+            guess = int.Parse(Console.ReadLine());
+
+            if (magicNumber > guess)
+            {
+                Console.WriteLine("Higher");
+            }
+            else if (magicNumber < guess)
+            {
+                Console.WriteLine("Lower");
+            }
+            else
+            {
+                Console.WriteLine("You guessed it!");
+            }
+        }
     }
+
 }
